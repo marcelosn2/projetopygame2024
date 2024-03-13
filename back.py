7a7a5d04e2a9c3ad278cb754cc6dd7633f23ac20
Marcelo Neves <marcelosn2@al.insper.edu.br>
2024-03-13 11:45:39 -0300

    funfa1

--- back.py
+++ back.py
@@ -3,25 +3,26 @@ import random
 
 
 class caixa:
-    def __init__(self, width, height):
+    def __init__(self, width):
         self.width = width
-        self.vel = 2
+        self.vel = 0.3
+        self.fonte = pygame.font.Font("assets/font/PressStart2P.ttf", 5)
+        self.pal = self.fonte.render("palavra", True, (0, 0, 0))
         self.caixa = self.pal.get_rect()
         # pygame.transform.scale(pygame.image.load("assets/img/caixa,png"), (20, 10))
-        self.pos = (random.randint(0, self.width), -30)
-        self.pal = self.fonte.render("palavra", True, (0, 0, 0))
+        self.pos = [random.randint(0, self.width), -30]
         # self.fonte_padrao = pygame.font.get_default_font()
-        self.fonte = pygame.font.Font("assets/font/PressStart2P.ttf", 20)
         print("Entrou")
 
-    def events(self):
-        # if jogo.recebe_eventos(self) == True:
-        # caixa.pos[1] += caixa.vel
-        return True
+    # def events(self):
+    #     if Jogo.recebe_eventos(self) == True:
+    #         caixa.pos[1] += caixa.vel
+    #     return True
 
     def desenha(self):
-        rectI = self.pal.get_rect()
-        pygame.draw.rect(self.pal, (255, 0, 0), rectI, 10)
+        # rectI = self.C.pal.get_rect()
+        # pygame.draw.rect(caixa.pal, (255, 0, 0), rectI, 10)
+        pass
 
 
 class Jogo:
@@ -31,7 +32,7 @@ class Jogo:
         self.height = height
         self.window = pygame.display.set_mode((self.width, self.height))
         self.vidas = 3
-        # caixa()
+        self.C = caixa(self.width)
 
     def teste(self):
         pygame.display.set_caption("GravWorddle")
@@ -40,15 +41,17 @@ class Jogo:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return False
-        caixa.events(self)
+        self.C.pos[1] += self.C.vel
+        if self.C.pos[1] > self.height:
+            self.C.pos[1] = -10
         return True
 
     def desenha(self):
         print("entroi desenha")
-        self.window.fill(255, 255, 255)
-        # caixa.desenha()
-        # self.window.blit(caixa.caixa, (caixa.pos[0], caixa.pos[1]))
-        # self.window.blit(caixa.pal, (caixa.pos[0], caixa.pos[1] - 10))
+        self.window.fill((255, 255, 255))
+        caixa.desenha(self)
+        # self.window.blit(self.C.caixa, (self.C.pos[0], self.C.pos[1]))
+        self.window.blit(self.C.pal, (self.C.pos[0], self.C.pos[1] - 10))
         pygame.display.flip()
 
     def game_loop(self):
