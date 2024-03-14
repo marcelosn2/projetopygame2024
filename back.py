572eb62a3f22626274ef98af692dd84e036bbb9e
Marcelo Neves <marcelosn2@al.insper.edu.br>
2024-03-14 00:06:06 -0300

    potos

--- back.py
+++ back.py
@@ -29,8 +29,17 @@ class caixa:
         return contents[level][number]
 
     def update(self):
+        r = self.fonte.render(self.text, True, (255, 255, 255))
+        self.pos[1] += self.vel
+        # if self.C.pos[1] > self.height + 10:
         if self.pos[1] >= 800:
+            if self.pal != r:
+                self.vel += 0.1
             self.pos[0] = random.randint(0, 900)
+            self.pal = self.fonte.render(self.get_word("1"), True, (0, 0, 0))
+            self.text = ""
+            self.pos[1] = -100
+            print(self.vel)
 
 
 class Jogo:
@@ -42,7 +51,6 @@ class Jogo:
         self.window = pygame.display.set_mode((self.width, self.height))
         self.vidas = 3
         self.C = caixa()
-        self.clock = pygame.time.Clock()
         #  self.retR = self.text.get_rect(center=(self.pos[0] + 150, self.pos[1] + 100))
 
         pygame.display.set_caption("GravWorddle")
@@ -61,29 +69,30 @@ class Jogo:
                 print(self.C.text)
             elif event.type == pygame.KEYDOWN:
                 self.C.text += str(event.unicode)
-        self.C.pos[1] += self.C.vel
-        if self.C.pos[1] > self.height:
-            self.C.pos[1] = -100
+
         return True
 
     def desenha(self):
         self.window.fill((255, 0, 0))
+        R = self.C.fonte.render(self.C.text, True, (255, 255, 255))
         self.window.blit(
             self.C.desenha("assets/imgs/img.png"),
             (self.C.pos[0], self.C.pos[1]),
         )
-        self.window.blit(self.C.pal, (self.C.pos[0] + 90, self.C.pos[1] + 30))
+        self.window.blit(self.C.pal, (self.C.pos[0] + 100, self.C.pos[1] + 30))
         # self.window.blit(self.R, self.C.retR)
-        self.window.blit(self.C.R, (self.C.pos[0] + 90, self.C.pos[1] + 130))
+        self.window.blit(R, (self.C.pos[0] + 110, self.C.pos[1] + 100))
         pygame.display.flip()
 
     def game_loop(self):
         x = Jogo.recebe_eventos
+        self.clock = pygame.time.Clock()
         self.clock.tick(60)
 
         while x:
+            caixa.update(self.C)
             Jogo.desenha(self)
             # caixa.desenha()
             x = Jogo.recebe_eventos(self)
-            caixa.update(self.C)
+            # caixa.update(self.C)
         return x
